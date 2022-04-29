81d54ddaa6c3672c2a25cc74020a30b5daf169cb
TooNMi <nmt1996bd@hotmail.com.vn>
2022-04-30 00:33:18 +0700

    Set up handlebars

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ let express = require('express');
 let app = express();
 
 
+//Set public static folder
+app.use(express.static(__dirname + '/public'));
+
+//Use view engine
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + 'views/partials/'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+
 //Set server port and start server
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
   console.log(`Server is running at port ${app.get('port')}`);
-})
+});
